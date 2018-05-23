249cb8f8de8e3a315bf0283cdbaf16fd5931734a
xuan <wendyxuanz@gmail.com>
2018-05-23 16:19:38 +0800

    fix bug

--- myo_raw.py
+++ myo_raw.py
@@ -287,8 +287,10 @@ class MyoRaw(object):
                 acc = vals[4:7]
                 gyro = vals[7:10]
                 self.on_imu(quat, acc, gyro)
+            # elif attr == 0x23:
+            #     typ, val, xdir = unpack('3B', pay)
             elif attr == 0x23:
-                typ, val, xdir = unpack('3B', pay)
+                typ, val, xdir, _, _, _ = unpack('6B', pay)
 
                 if typ == 1: # on arm
                     self.on_arm(Arm(val), XDirection(xdir))
